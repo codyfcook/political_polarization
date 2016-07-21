ac1aa0238345b43f12ea6d2c3155498cada816aa
Cody Cook <c_cook@GreatestGood.com>
2016-07-21 10:44:37 -0500

    code cleanup

--- Old code/rundirectory.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-subprocess.call("python scrape_senate.py", shell=True)
-subprocess.call("python scrape_house.py", shell=True)
-subprocess.call("python convert_to_csv.py", shell=True)
-
